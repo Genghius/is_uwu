0170d5f12d7e2071d11afca2f41d422f7a8e9a27
Genghius <genghius.khanate01@gmail.com>
2020-08-06 23:46:43 +0100

    made it be able to verify uwu'ness of strings.

---
+++ is_uwu.js
@@ -0,0 +1,10 @@
+function is_uwu(wannabeuwu)			{
+let uwu = /uwu/ig				;
+	if(uwu.test(wannabeuwu))		{
+		return true			;
+						} 
+	else 					{
+		return false			;
+						}
+						}
+exports.is_uwu = is_uwu				;
